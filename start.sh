d4b51288737c8003d088399b53f2e181aa3e8a0b
Juan Scerri <juan.scerri.js@gmail.com>
2022-12-26 13:31:14 +0100

    update start.sh and shutdown.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 
 docker-compose up -d
 echo "Wait for microservices to register themselves..."
+<<<<<<< Updated upstream
 sleep 20 # give the microservices enough time
+=======
+sleep 25 # give the microservices enough time
+>>>>>>> Stashed changes
 cd interface
-npm run start
+npm run start || npm install --force && npm run start
 cd ..
