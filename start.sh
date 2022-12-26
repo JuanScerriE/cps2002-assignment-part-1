4e8ea7b083d0002991c4779dc1214cd440d17682
Juan Scerri <juan.scerri.js@gmail.com>
2022-12-26 13:38:30 +0100

    incorrect syntax

--- start.sh
+++ start.sh
@@ -2,11 +2,7 @@
 
 docker-compose up -d
 echo "Wait for microservices to register themselves..."
-<<<<<<< Updated upstream
-sleep 20 # give the microservices enough time
-=======
 sleep 25 # give the microservices enough time
->>>>>>> Stashed changes
 cd interface
 npm run start || npm install --force && npm run start
 cd ..
